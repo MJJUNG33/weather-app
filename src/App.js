c52a686e9f9595972579e3764e3270941a3e5b0c
Minji Jung <jung2674@gmail.com>
2024-03-25 02:37:18 +1300

    Add button onclick functions that show weather info of current location and other cities

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import DisplayBox from "./component/DisplayBox";
 import LocationButtons from "./component/LocationButtons";
 import "bootstrap/dist/css/bootstrap.min.css";
+import { Spinner } from "react-bootstrap";
 
 //To do
 // 1. Current location's weather on landing page.
@@ -14,6 +15,10 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   const [currentWeather, setCurrentWeather] = useState(null);
+  const [city, setCity] = useState("");
+  const [loading, setLoading] = useState(false);
+  const [activeButton, setActiveButton] = useState(null);
+  const cities = ["Seoul", "London", "New york", "Tokyo"];
 
   const getCurrentLocation = () => {
     navigator.geolocation.getCurrentPosition((position) => {
@@ -24,23 +29,59 @@ function App() {
   };
 
   const fetchWeatherByCurrentLocation = async (latitude, longitude) => {
+    setLoading(true);
     const response = await fetch(
       `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=7c41da7881d71baf9bc475e871b9534c&units=metric`
     );
+
     const data = await response.json();
     console.log(data);
     setCurrentWeather(data);
+    setLoading(false);
+  };
+
+  const fetchWeatherByCity = async () => {
+    setLoading(true);
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=7c41da7881d71baf9bc475e871b9534c&units=metric`
+    );
+
+    const data = await response.json();
+    setCurrentWeather(data);
+    console.log(data);
+    setLoading(false);
   };
 
   useEffect(() => {
-    getCurrentLocation();
-  }, []);
+    if (!city) {
+      getCurrentLocation();
+    } else {
+      fetchWeatherByCity();
+    }
+  }, [city]);
 
   return (
-    <div className="App">
-      <DisplayBox currentWeather={currentWeather} />
-      <LocationButtons />
-    </div>
+    <>
+      {loading ? (
+        <div className="App">
+          <Spinner animation="border" variant="info">
+            <span className="visually-hidden">Loading...</span>
+          </Spinner>
+        </div>
+      ) : (
+        <div className="App">
+          <DisplayBox currentWeather={currentWeather} />
+          <LocationButtons
+            cities={cities}
+            setCity={setCity}
+            setCurrentWeather={setCurrentWeather}
+            activeButton={activeButton}
+            setActiveButton={setActiveButton}
+            getCurrentLocation={getCurrentLocation}
+          />
+        </div>
+      )}
+    </>
   );
 }
 
--- src/component/LocationButtons.js
+++ src/component/LocationButtons.js
@@ -1,13 +1,45 @@
 import React from "react";
 
-const LocationButtons = () => {
+const LocationButtons = ({
+  cities,
+  setCity,
+  activeButton,
+  setActiveButton,
+  getCurrentLocation,
+}) => {
+  console.log(cities);
+
   return (
     <div className="locationButtons">
-      <button className="currentLocationButton">Current Location</button>
-      <button className="anotherLocationButton">Seoul</button>
-      <button className="anotherLocationButton">London</button>
-      <button className="anotherLocationButton">New york</button>
-      <button className="anotherLocationButton">Melbourne</button>
+      <button
+        className={`currentLocationButton ${
+          activeButton === "" ? "active" : ""
+        }`}
+        onClick={() => {
+          setCity("");
+          getCurrentLocation();
+          setActiveButton("");
+        }}
+      >
+        Current Location
+      </button>
+      {cities.map((city, i) => {
+        return (
+          <button
+            key={i}
+            className={`anotherLocationButton ${
+              activeButton === city ? "active" : ""
+            }`}
+            onClick={() => {
+              setCity(city);
+              setActiveButton(city);
+              console.log(city);
+            }}
+          >
+            {city}
+          </button>
+        );
+      })}
     </div>
   );
 };
